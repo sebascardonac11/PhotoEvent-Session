a7956491061359f808e388d0c2adf753d87ddbf9
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-06 14:20:10 -0500

    Agregando decoded al put

--- functions/session.js
+++ functions/session.js
@@ -68,9 +68,7 @@ module.exports = class Session {
     }
     async putPhoto(email, data, fileName) {
         try {
-            const parsedBody=JSON.parse(data);
-            const base64File =parsedBody.file;
-            const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/,""),'base64');
+            const decodedFile = Buffer.from(data.replace(/^data:image\/\w+;base64,/,""),'base64');
             const params = {
                 Bucket: 'photoevent/photoClient',
                 Body: decodedFile,
