7c7ab893a1239b608291a89d407874e73ac0429e
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-10-24 16:45:19 -0500

    Set Response error

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ exports.handler = async function (event, context, callback) {
     //console.log("Event: ", event);
     var session = new Session(process.env.BUCKET, process.env.DYNAMODB);
     //var session = new Session('photoeventdev', 'photoEvent');
+    var response = { statusCode: 401, data: "Whitout Information" };
     var authorizationDecoded = jwt_decode(event.headers.Authorization);
     switch (event.httpMethod) {
       case 'GET':
         if (event.resource == '/photoEvent-sessions/person') {
-          this.response = await session.getPersons(event.queryStringParameters.session)
+          response = await session.getPersons(event.queryStringParameters.session)
         }
         if (event.resource == '/photoEvent-sessions') {
-          this.response = await session.getSessions(authorizationDecoded.email, event.queryStringParameters.event);
+          response = await session.getSessions(authorizationDecoded.email, event.queryStringParameters.event);
         }
         break;
       case 'PUT':
@@ -23,25 +24,25 @@ exports.handler = async function (event, context, callback) {
           var key = form.event + '/' + form.session + '/' + form.files[0].filename;
           var contenType = form.files[0].contentType;
           var body = Buffer.from(form.files[0].content);
-          this.response = await session.putPhoto(key, contenType, body, authorizationDecoded.email);
+          response = await session.putPhoto(key, contenType, body, authorizationDecoded.email);
         break;
       case 'POST':
         //console.log("### POST ####")
-        this.response = await session.setSession(event.body, authorizationDecoded.email);
+        response = await session.setSession(event.body, authorizationDecoded.email);
         break;
       default:
       // code
     }
-    console.log("Response: ", this.response);
+    console.log("Response: ", response);
     return {
-      statusCode: this.response.statusCode,
+      statusCode: response.statusCode,
       headers: {
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
         "Content-Type": 'application/json'
       },
-      body: JSON.stringify(this.response.data)
+      body: JSON.stringify(response.data)
     };
   } catch (error) {
     console.log("!!!!!!!!Something wrong:",error);
